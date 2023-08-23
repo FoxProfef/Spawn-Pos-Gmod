d17bf24f8a9700c90d7348647d657c7eb387b5d9
FoxProfef <76404941+FoxProfef@users.noreply.github.com>
2023-08-23 09:13:48 +0800

    частично оптимизированный код

--- Spawn Pos/lua/autorun/client/cl_spawn_pos.lua
+++ Spawn Pos/lua/autorun/client/cl_spawn_pos.lua
@@ -1,43 +1,71 @@
-SpawnTable={}
-surface.CreateFont( "Waypoint_big", {
-    font = "DermaLarge",
-    size = 600,
-    weight = 5000,
-    antialias = true,
-} )
+local SpawnTable = {}
+surface.CreateFont(
+    "Waypoint_big",
+    {
+        font = "DermaLarge",
+        size = 600,
+        weight = 5000,
+        antialias = true,
+    }
+)
+
+local color1 = Color(255, 255, 255)
+local color2 = Color(0, 0, 0, 150)
+local surface_SetFont = surface.SetFont
+local surface_GetTextSize = surface.GetTextSize
+local xy = 110
+local xy2 = -(xy / 2)
+local xy4 = -(xy * 4)
+local cam_Start3D2D = cam.Start3D2D
+local cam_IgnoreZ = cam.IgnoreZ
+local draw_RoundedBox = draw.RoundedBox
+local draw_WordBox = draw.WordBox
+local cam_End3D2D = cam.End3D2D
+local pairs=pairs
 local function waypoint_draw()
+    local angles = LocalPlayer():EyeAngles()
+    if not SpawnTable then
+        hook.Remove('PostDrawOpaqueRenderables', 'spawn_fox_hide')
+        return
+    end
+
     for k, v in pairs(SpawnTable) do
         if not k then continue end
-        local angles = LocalPlayer():EyeAngles()
         local text = "[" .. k .. "]"
-        surface.SetFont("Waypoint_big")
-        local TextWidth_wp = surface.GetTextSize(text)
-        local xy = 10 + 100
+        surface_SetFont("Waypoint_big")
+        local TextWidth_wp = surface_GetTextSize(text)
         angles:RotateAroundAxis(angles:Forward(), 90)
         angles:RotateAroundAxis(angles:Right(), 90)
-        cam.Start3D2D(Vector(v), angles, 0.1)
-        cam.IgnoreZ(true)
-        draw.RoundedBox(80, -xy / 2, (-xy / 2) * 2, xy, xy, Color(255, 255, 255))
-        draw.WordBox(2, -TextWidth_wp * 0.5, -xy * 4, text, "Waypoint_big", Color(0, 0, 0, 150), Color(255, 255, 255))
-        cam.IgnoreZ(false)
-        cam.End3D2D()
+        cam_Start3D2D(Vector(v), angles, 0.1)
+        cam_IgnoreZ(true)
+        draw_RoundedBox(80, xy2, -xy, xy, xy, color1)
+        draw_WordBox(2, -TextWidth_wp * 0.5, xy4, text, "Waypoint_big", color2, color1)
+        cam_IgnoreZ(false)
+        cam_End3D2D()
     end
 end
-net.Receive("spawn_show_reload_fox", function()
-    if LocalPlayer().Bool then
+
+net.Receive(
+    "spawn_show_reload_fox",
+    function()
+        --if LocalPlayer().Bool then
         SpawnTable = net.ReadTable()
         hook.Remove('PostDrawOpaqueRenderables', 'spawn_fox_hide')
         hook.Add('PostDrawOpaqueRenderables', 'spawn_fox_hide', waypoint_draw)
+        LocalPlayer().Bool = true
     end
-end)
+)
 
-net.Receive("spawn_show_fox", function()
-    LocalPlayer().Bool = net.ReadBool()
-    SpawnTable = net.ReadTable()
-
-    if LocalPlayer().Bool then
-        hook.Add('PostDrawOpaqueRenderables', 'spawn_fox_hide', waypoint_draw)
-    else
-        hook.Remove('PostDrawOpaqueRenderables', 'spawn_fox_hide')
+--end
+net.Receive(
+    "spawn_show_fox",
+    function()
+        LocalPlayer().Bool = net.ReadBool()
+        SpawnTable = net.ReadTable()
+        if LocalPlayer().Bool then
+            hook.Add('PostDrawOpaqueRenderables', 'spawn_fox_hide', waypoint_draw)
+        else
+            hook.Remove('PostDrawOpaqueRenderables', 'spawn_fox_hide')
+        end
     end
-end)
+)
--- Spawn Pos/lua/autorun/server/sv_spawn_pos.lua
+++ Spawn Pos/lua/autorun/server/sv_spawn_pos.lua
@@ -1,42 +1,40 @@
-TableVec = {}
-TableAng = {}
-
+local TableVec = {}
+local TableAng = {}
+local tonumber = tonumber
 function SQLQuery(queryStr, func, singleRow)
     local query
-
-    if (not singleRow) then
+    if not singleRow then
         query = sql.Query(queryStr)
     else
         query = sql.QueryRow(queryStr, 1)
     end
 
-    if (query == false) then
+    if query == false then
         print("[SQLLite] ERROR", sql.LastError())
-    elseif (func) then
+    elseif func then
         func(query)
     end
 end
 
-if (not sql.TableExists("players_spawn_pos")) then
-    SQLQuery([[ CREATE TABLE players_spawn_pos ( 
-	map text,
-	mapid int,
-		x int,
-		y int,
-		z int,
-		ax int,
-		ay int,
-		az int
-	); ]])
-end
-
+if not sql.TableExists("players_spawn_pos") then SQLQuery([[ CREATE TABLE players_spawn_pos ( 
+    map text,
+    mapid int,
+        x int,
+        y int,
+        z int,
+        ax int,
+        ay int,
+        az int
+    ); ]]) end
+local mapid
+local game_GetMap = game.GetMap
 local function Mapid()
     local mapidtabl = 0
-    mapid = tonumber(sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game.GetMap() .. "'"))
-    local val = sql.Query("SELECT mapid FROM players_spawn_pos WHERE map ='" .. game.GetMap() .. "'  LIMIT " .. mapid .. ";")
-
+    mapid = tonumber(sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game_GetMap() .. "'"))
+    local val = sql.Query("SELECT mapid FROM players_spawn_pos WHERE map ='" .. game_GetMap() .. "'  LIMIT " .. mapid .. ";")
     if val then
-        for k, v in pairs(val) do
+        for i = 1, #val do
+            local v = val[i]
             if tonumber(v.mapid) > 0 and tonumber(v.mapid) > tonumber(mapidtabl) then
                 mapidtabl = v.mapid
                 mapid = v.mapid
@@ -45,10 +43,9 @@ local function Mapid()
     end
 
     mapid = mapid + 1
-
     for i = 1, mapid do
-        if sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game.GetMap() .. "' and mapid=" .. i .. ";") ~= '0' then
-            i2 = tonumber(sql.QueryRow("SELECT * from players_spawn_pos WHERE map ='" .. game.GetMap() .. "' and mapid = " .. i .. "").mapid)
+        if sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game_GetMap() .. "' and mapid=" .. i .. ";") ~= '0' then
+            i2 = tonumber(sql.QueryRow("SELECT * from players_spawn_pos WHERE map ='" .. game_GetMap() .. "' and mapid = " .. i .. "").mapid)
         else
             i2 = 1
         end
@@ -58,14 +55,12 @@ local function Mapid()
             break
         end
     end
-
     return mapid
 end
 
 Mapid()
-
 local function Mapid_add()
-    if sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game.GetMap() .. "' and mapid=" .. 1 .. ";") == '0' then
+    if sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game_GetMap() .. "' and mapid=" .. 1 .. ";") == '0' then
         return 1
     else
         return Mapid()
@@ -76,11 +71,12 @@ local function LoadSpawnPos()
     local mapidtabl = 0
     TableVec = {}
     TableAng = {}
-    mapid2 = tonumber(sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game.GetMap() .. "'"))
-    local val = sql.Query("SELECT mapid FROM players_spawn_pos WHERE map ='" .. game.GetMap() .. "'  LIMIT " .. mapid2 .. ";")
-
+    mapid2 = tonumber(sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game_GetMap() .. "'"))
+    local val = sql.Query("SELECT mapid FROM players_spawn_pos WHERE map ='" .. game_GetMap() .. "'  LIMIT " .. mapid2 .. ";")
     if val then
-        for k, v in pairs(val) do
+        -- for k, v in pairs(val) do
+        for i = 1, #val do
+            local v = val[i]
             if tonumber(v.mapid) > 0 and tonumber(v.mapid) > tonumber(mapidtabl) then
                 mapidtabl = v.mapid
                 mapid2 = v.mapid
@@ -89,120 +85,150 @@ local function LoadSpawnPos()
     end
 
     for i = 1, mapid2 do
-        SQLQuery("SELECT * FROM players_spawn_pos WHERE mapid = " .. i .. " and map ='" .. game.GetMap() .. "';", function(d)
-            if d then
-                if d.map == game.GetMap() then
-                    TableVec[i] = Vector(d.x, d.y, d.z)
-                    TableAng[i] = Angle(d.ax, d.ay, d.az)
+        SQLQuery(
+            "SELECT * FROM players_spawn_pos WHERE mapid = " .. i .. " and map ='" .. game_GetMap() .. "';",
+            function(d)
+                if d then
+                    if d.map == game_GetMap() then
+                        TableVec[i] = Vector(d.x, d.y, d.z)
+                        TableAng[i] = Angle(d.ax, d.ay, d.az)
+                    end
                 end
-            end
-        end, true)
+            end,
+            true
+        )
     end
-    --
 end
 
+--
 LoadSpawnPos()
 util.AddNetworkString("spawn_show_reload_fox")
-
-function ReloadSpawnHud(pl)
-    net.Start("spawn_show_reload_fox")
-    net.WriteTable(TableVec)
-    net.Broadcast()
+local net_Start = net.Start
+local net_WriteTable = net.WriteTable
+local net_Send = net.Send
+function ReloadSpawnHud()
+    local pla = player.GetAll()
+    for i = 1, #pla do
+        if not pla[i].spawn_show then continue end
+        net_Start("spawn_show_reload_fox")
+        net_WriteTable(TableVec)
+        net_Send(pla[i])
+    end
 end
 
-concommand.Add('spawn_add', function(pl)
-    if not pl:IsSuperAdmin() then
-        pl:PrintMessage(HUD_PRINTTALK, "Ты не Админ!")
+concommand.Add(
+    'spawn_add',
+    function(pl)
+        if not pl:IsSuperAdmin() then
+            pl:PrintMessage(HUD_PRINTTALK, "Ты не Админ!")
+            return
+        end
 
-        return
+        pl:PrintMessage(HUD_PRINTTALK, "Точка Спавна Добавленна!")
+        local pos = pl:GetPos()
+        local ang = pl:EyeAngles()
+        local v = Vector(pos.x, pos.y, pos.z)
+        local a = Angle(ang.x, ang.y, ang.z)
+        Mapid()
+        TableVec[mapid] = v
+        TableAng[mapid] = a
+        SQLQuery("INSERT INTO players_spawn_pos(map,mapid,x,y,z,ax,ay,az) VALUES('" .. game_GetMap() .. "'," .. Mapid_add() .. ',' .. pos.x .. ',' .. pos.y .. ',' .. pos.z .. ',' .. ang.x .. ',' .. ang.y .. ',' .. ang.z .. ");")
+        --LoadSpawnPos()
+        ReloadSpawnHud()
     end
+)
 
-    pl:PrintMessage(HUD_PRINTTALK, "Точка Спавна Добавленна!")
-    local pos = pl:GetPos()
-    local ang = pl:EyeAngles()
-    local v = (Vector(pos.x, pos.y, pos.z))
-    local a = (Angle(ang.x, ang.y, ang.z))
-    TableVec[#TableVec + 1] = v
-    TableAng[#TableAng + 1] = a
-	
+concommand.Add(
+    'spawn_reset',
+    function(pl)
+        if not pl:IsSuperAdmin() then
+            pl:PrintMessage(HUD_PRINTTALK, "Ты не Админ!")
+            return
+        end
 
-    SQLQuery("INSERT INTO players_spawn_pos(map,mapid,x,y,z,ax,ay,az) VALUES('" .. game.GetMap() .. "'," .. Mapid_add() .. ',' .. pos.x .. ',' .. pos.y .. ',' .. pos.z .. ',' .. ang.x .. ',' .. ang.y .. ',' .. ang.z .. ");")
-    --LoadSpawnPos()
-    ReloadSpawnHud()
-end)
-
-concommand.Add('spawn_reset', function(pl)
-    if not pl:IsSuperAdmin() then
-        pl:PrintMessage(HUD_PRINTTALK, "Ты не Админ!")
-
-        return
+        pl:PrintMessage(HUD_PRINTTALK, "Все точки спавна удалены!")
+        SQLQuery("DELETE FROM players_spawn_pos WHERE map = '" .. game_GetMap() .. "';")
+        TableVec = {}
+        TableAng = {}
+        ReloadSpawnHud()
     end
-    pl:PrintMessage(HUD_PRINTTALK, "Все точки спавна удалены!")
-    SQLQuery("DELETE FROM players_spawn_pos WHERE map = '" .. game.GetMap() .. "';")
-    TableVec = {}
-    TableAng = {}
-    ReloadSpawnHud()
-end)
+)
 
-concommand.Add('spawn_remove', function(pl, cmd, args)
-    if not pl:IsSuperAdmin() then
-        pl:PrintMessage(HUD_PRINTTALK, "Ты не Админ!")
+concommand.Add(
+    'spawn_remove',
+    function(pl, cmd, args)
+        if not pl:IsSuperAdmin() then
+            pl:PrintMessage(HUD_PRINTTALK, "Ты не Админ!")
+            return
+        end
 
-        return
+        if not args[1] then return end
+        local num = tonumber(args[1])
+        if sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game_GetMap() .. "' and mapid=" .. num .. ";") == '0' then
+            pl:PrintMessage(HUD_PRINTTALK, "Такой точки спавна нет!")
+            return
+        end
+
+        TableVec[num] = nil
+        TableAng[num] = nil
+        pl:PrintMessage(HUD_PRINTTALK, "Точка " .. args[1] .. " была удалена!")
+        SQLQuery("DELETE FROM players_spawn_pos WHERE map = '" .. game_GetMap() .. "' and mapid=" .. args[1] .. ";")
+        ReloadSpawnHud()
     end
-
-    if not args[1] then return end
-    local num = tonumber(args[1])
-
-    if sql.QueryValue("SELECT count(*) from players_spawn_pos WHERE map ='" .. game.GetMap() .. "' and mapid=" .. num .. ";") == '0' then
-        pl:PrintMessage(HUD_PRINTTALK, "Такой точки спавна нет!")
-
-        return
-    end
-
-    TableVec[num] = nil
-    TableAng[num] = nil
-    pl:PrintMessage(HUD_PRINTTALK, "Точка " .. args[1] .. " была удалена!")
-    SQLQuery("DELETE FROM players_spawn_pos WHERE map = '" .. game.GetMap() .. "' and mapid=" .. args[1] .. ";")
-    --LoadSpawnPos()
-    ReloadSpawnHud()
-end)
+)
 
 util.AddNetworkString("spawn_show_fox")
-
-concommand.Add('spawn_show', function(pl, cmd, args)
-    if not pl.show then
-        pl:PrintMessage(HUD_PRINTTALK, "Теперь вы видите точки спавна")
-        pl.show = true
-        net.Start("spawn_show_fox")
-        net.WriteBool(true)
-        net.WriteTable(TableVec)
-        net.Send(pl)
-    else
-        pl:PrintMessage(HUD_PRINTTALK, "Вы больше не видите точки спавна!")
-        pl.show = false
-        net.Start("spawn_show_fox")
-        net.WriteBool(false)
-        net.Send(pl)
-    end
-end)
-hook.Add('PlayerSpawn', 'Spawn_Pos', function(ply)
-    for i = 1, #TableVec do
-        p = math.random(1, #TableVec)
-        if not TableVec[p] then continue end
-
-        if p ~= ply.LastSpawnFox then
-            ply.LastSpawnFox = p
-            break
+concommand.Add(
+    'spawn_show',
+    function(pl, cmd, args)
+        if not pl.spawn_show then
+            pl:PrintMessage(HUD_PRINTTALK, "Теперь вы видите точки спавна")
+            pl.spawn_show = true
+            net.Start("spawn_show_fox")
+            net.WriteBool(true)
+            net.WriteTable(TableVec)
+            net.Send(pl)
+        else
+            pl:PrintMessage(HUD_PRINTTALK, "Вы больше не видите точки спавна!")
+            pl.spawn_show = false
+            net.Start("spawn_show_fox")
+            net.WriteBool(false)
+            net.Send(pl)
         end
     end
+)
 
-    if p then
+local math_random = math.random
+hook.Add(
+    'PlayerSpawn',
+    'Spawn_Pos',
+    function(ply)
+        local last = ply.LastSpawnFox or 0
+        local p = last
+        local Num = #TableVec
+        if Num == 0 then return end
+        if Num > 1 then
+            while p == last do
+                p = math_random(Num)
+                if p ~= last then
+                    ply.LastSpawnFox = p
+                    break
+                end
+            end
+        else
+            for k, v in pairs(TableVec) do
+                p = k
+                break
+            end
+        end
+
+        PrintTable(TableVec)
+
+        if not p or p == nil then return end
         selectv = TableVec[p]
         selecta = TableAng[p]
         ply:SetPos(selectv)
         ply:SetEyeAngles(selecta)
         p = nil
     end
-
-end)
+)
